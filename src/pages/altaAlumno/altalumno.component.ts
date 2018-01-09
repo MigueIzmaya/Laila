46e63dfd4768b11f0578478d0c2e46c596a87f2c
Izmaya <m.izmaya@hotmail.com>
2018-01-09 04:41:02 -0600

    nuevas paginas

--- src/pages/altaAlumno/altalumno.component.ts
+++ src/pages/altaAlumno/altalumno.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-altalumno',
@@ -7,8 +9,22 @@ import { NavController } from 'ionic-angular';
 })
 export class AltaAlumno {
 
-  constructor(public navCtrl: NavController) {
+  boleta: string;
+  nombre: string;
 
+  registerAlumno = {boleta: '', nombre: ''};
+  items: Observable<any[]>;
+
+  constructor(public navCtrl: NavController, public afDB: AngularFireDatabase) {
+
+  }
+
+  registrar_alumno(){
+    //this.items = this.afDB.list('/Actividad').valueChanges();
+    const ref1 = this.afDB.list('/Alumno').query.ref.push();
+    ref1.set(this.registerAlumno.boleta);
+    console.log(ref1.key);
+    //console.log(newKey);
   }
 
 }
