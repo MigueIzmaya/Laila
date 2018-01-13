c9196e664f0dbb615c815bd94dae197567c833c0
Izmaya <m.izmaya@hotmail.com>
2018-01-13 12:51:40 -0600

    Bluetooth

--- src/pages/bluetooth/bluetooth.component.ts
+++ src/pages/bluetooth/bluetooth.component.ts
@@ -10,50 +10,43 @@ export class Bluetooth {
   devices:any;
 pages: Array<{title: string, component: any}>;
   constructor(public navCtrl: NavController, private bluetoothSerial: BluetoothSerial) {
-    //this.devices = "";
     this.devices = "Presiona para buscar";
-
-
   }
 
   buscar() {
-    //this.devices="lala";
     this.bluetoothSerial.isEnabled().then(device=>{
       this.isConnect();
     }).catch(connect=>{
       this.devices = "El bluetooth está desactivado";
     });
-
-
-    /*this.bluetoothSerial.connect("00:06:66:80:AD:70").map
-    this.bluetoothSerial.list().then(device=>{
-      //this.devices.push(device);
-      //console.log(device);
-      this.devices = device;
-      //this.devices.push(device);
-
-    });*/
   }
 
   isConnect(){
-    this.devices="Conectando...";
-
     this.bluetoothSerial.isConnected().then(device =>{
-      this.devices=device;
+      this.read();
     }).catch(device=>{
-      this.devices=device;
-    });
-
-    this.bluetoothSerial.connect("00:06:66:80:AD:70").subscribe(peripheralData =>{
-      this.devices = "Conectado";
-      this.bluetoothSerial.write('Hola Mundo').then(device=>{
-        this.devices = "Si escribi";
-      }).catch(device=>{
-        this.devices = device;
-      });
+      this.connect();
     });
   }
 
+  connect(){
+    this.bluetoothSerial.connect("00:06:66:80:AD:70").subscribe(peripheralData =>{
+      this.devices = "Conectado";
+    });
+  }
 
+  write(){
+    this.bluetoothSerial.write('Hola Mundo').then(device=>{
+      this.devices = "Si escribi";
+    }).catch(device=>{
+      this.devices = device;
+    });
+  }
 
+  read(){
+    this.bluetoothSerial.read().then(peripheralData => {
+      this.devices = peripheralData;
+    })
+
+  }
 }
