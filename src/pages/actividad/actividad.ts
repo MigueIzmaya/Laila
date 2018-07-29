ed3ccb5d0630bfdaa7dcd62c53a50d0f27be75d1
MigueIzmaya <m.izmaya@hotmail.com>
2018-07-29 16:21:46 -0500

    Ingresando el alta de actividades

--- src/pages/actividad/actividad.ts
+++ src/pages/actividad/actividad.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TasksServiceProvider } from '../../providers/tasks-service/tasks-service';
-
+import { AlertController } from 'ionic-angular';
 
 
 @Component({
@@ -20,23 +20,37 @@ export class Actividad {
   registerActivityDataBase = {miDia: '', miHora: '', activity: '', alumno: ''};
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
+              public alertCtrl: AlertController,
               public tasksService: TasksServiceProvider) {
                 this.getAllAlumnos();
 
   }
 
   registrar_actividad(){
+
+    /*ActividadAlumno(id_actividadAlumno INTEGER PRIMARY KEY AUTOINCREMENT, fechaInicio TEXT, duracion INTEGER, calificacion INTEGER, Actividad_idActividad INTEGER, boleta TEXT, FOREIGN KEY(Actividad_idActividad) REFERENCES Actividad(id_actividad)
     this.registerAlumnoDatabase.boleta = this.registerActivity.activity;
     this.registerAlumnoDatabase.nombre = this.registerActivity.nombre;
     this.registerAlumnoDatabase.numero_serie = null;
     this.registerAlumnoDatabase.usuario = this.nombreUsuarioMaestro;
-    this.tasksService.insertTableAlumno(this.registerAlumnoDatabase);
+    this.tasksService.insertTableAlumno(this.registerAlumnoDatabase);*/
+    this.showAlert("AltaActividad",this.registerActivity.miDia,"Aceptar");
+    this.showAlert("AltaActividad",this.registerActivity.miHora,"Aceptar");
   }
 
   getAllAlumnos(){
     this.tasksService.getAllAlumnos().then(alumnos=>{
       this.Alumnos = alumnos;
     });
+  }
+
+  showAlert(titulo, contenido, boton) {
+    let alert = this.alertCtrl.create({
+      title: titulo,
+      subTitle: contenido,
+      buttons: [boton]
+    });
+    alert.present();
   }
 
 
